0e0eec48aea7d2cc3ddb052a1c6bc9d8317e783e
tscp <45612704+tddschn@users.noreply.github.com>
2021-03-01 20:30:01 +0800

    2021-03-01T12-30-01Z

--- Casks/clashx-pro.rb
+++ Casks/clashx-pro.rb
@@ -1,10 +1,10 @@
 cask "clashx-pro" do
-  version "1.31.2.1"
-  sha256 "f0ce1ae36ab256ec58b136c74687eee2d689fdc662fdd1ba6973d49817f3c810"
+  version "1.40.0.1"
+  sha256 "4f9fb11bf8118478e1f3e52c951cc3f6a6e618c043377f6e0e543656344f59dd"
 
   # the domain of url may vary! fix the comment when updating this file.
   # appcenter-filemanagement-distrib1ede6f06e.azureedge.net was verified as official when first introduced to the cask
-  url "https://appcenter-filemanagement-distrib5ede6f06e.azureedge.net/591550b0-9ccd-4877-8e33-b1c003c34431/ClashX.dmg?sv=2019-02-02&sr=c&sig=LUO9C2zsrb3dbttUOyLGJg4tnQtLH2q%2B760vKZ3Ze1A%3D&se=2021-01-07T21%3A02%3A54Z&sp=r&download_origin=appcenter"
+  url "https://appcenter-filemanagement-distrib1ede6f06e.azureedge.net/3da8d480-6af1-4498-9696-276ec3a3cd3f/ClashX.dmg?sv=2019-02-02&sr=c&sig=mA5aMwcIp3XeBLOHNJbH5mm%2FvuHqWJXhcrMz8J8eiZs%3D&se=2021-03-02T00%3A27%3A58Z&sp=r&download_origin=appcenter"
   name "ClashX Pro"
   desc "Rule-based custom proxy with GUI based on clash with pro clash core"
   homepage "https://install.appcenter.ms/users/clashx/apps/clashx-pro/distribution_groups/public"
